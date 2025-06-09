69ba8b22cf6d2cc0a09033bb14b3cf6e933fef76
suneel kumar <sunil.kumar987654433@gmail.com>
2025-06-09 13:10:38 +0530

    update in admin and create single migrations file

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from account.models import CustomUser
 # Register your models here.
+
+
+# admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', "email", "full_name"]
+    search_fields = ['first_name', 'last_name', 'email']
+
+
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from book.models import Instructor, BookingSlot, FitnessClass
 # Register your models here.
+
+admin.site.register(( BookingSlot, FitnessClass))
+
+
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ['id', "user_instructor", "user_instructor_name", "user_instructor_email", "experience"]
+    search_fields = ['user_instructor_email', ]
+
+
+    def user_instructor_email(self, obj):
+        return f"{obj.user_instructor.email}"
+    
+    def user_instructor_name(self, obj):
+        return f"{obj.user_instructor.first_name}"
